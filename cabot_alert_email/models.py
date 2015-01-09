a893b1903f102e4d5c5aa69b5bfed83c90e715d4
Jonathan Balls <j.balls@btopenworld.com>
2015-01-09 08:42:44 +0000

    initial commit

---
+++ cabot_alert_email/models.py
@@ -0,0 +1,50 @@
+from os import environ as env
+
+from django.conf import settings
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.template import Context, Template
+
+from cabot.cabotapp.alert import AlertPlugin
+
+import requests
+import logging
+
+email_template = """Service {{ service.name }} {{ scheme }}://{{ host }}{% url 'service' pk=service.id %} {% if service.overall_status != service.PASSING_STATUS %}alerting with status: {{ service.overall_status }}{% else %}is back to normal{% endif %}.
+{% if service.overall_status != service.PASSING_STATUS %}
+CHECKS FAILING:{% for check in service.all_failing_checks %}
+  FAILING - {{ check.name }} - Type: {{ check.check_category }} - Importance: {{ check.get_importance_display }}{% endfor %}
+{% if service.all_passing_checks %}
+Passing checks:{% for check in service.all_passing_checks %}
+  PASSING - {{ check.name }} - Type: {{ check.check_category }} - Importance: {{ check.get_importance_display }}{% endfor %}
+{% endif %}
+{% endif %}
+"""
+
+class EmailAlert(AlertPlugin):
+    name = "Email Plugin"
+    author = "Jonathan Balls"
+
+    def send_alert(self, service, users, duty_officers):
+        emails = [u.email for u in users if u.email]
+        if not emails:
+            return
+        c = Context({
+            'service': service,
+            'host': settings.WWW_HTTP_HOST,
+            'scheme': settings.WWW_SCHEME
+        })
+        if service.overall_status != service.PASSING_STATUS:
+            if service.overall_status == service.CRITICAL_STATUS:
+                emails += [u.email for u in duty_officers]
+            subject = '%s status for service: %s' % (
+                service.overall_status, service.name)
+        else:
+            subject = 'Service back to normal: %s' % (service.name,)
+        t = Template(email_template)
+        send_mail(
+            subject=subject,
+            message=t.render(c),
+            from_email='Cabot <%s>' % settings.CABOT_FROM_EMAIL,
+            recipient_list=emails,
+        )
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cabot_alert_email',
+      version='1.0',
+      description='An email alert plugin for Cabot by Arachnys',
+      author='Arachnys',
+      author_email='info@arachnys.org',
+      url='http://cabotapp.com',
+      packages=[
+      	'cabot_alert_email'
+      	],
+     )
