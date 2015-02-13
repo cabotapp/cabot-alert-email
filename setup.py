7cc370b349f863dbe34dca55277c9036cc5e8135
Jonathan Balls <j.balls@btopenworld.com>
2015-02-13 14:40:27 +0000

    fixed email address finding bug

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-email',
-      version='1.0',
+      version='1.0.1',
       description='An email alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url= 'https://github.com/bonniejools/cabot-alert-email/tarball/0.1'
+      download_url= 'https://github.com/bonniejools/cabot-alert-email/tarball/1.0.1'
      )
