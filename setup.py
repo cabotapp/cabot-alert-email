06d2abe95d349a6ce2f76ee56f14af615a4d6c12
Frank Hamand <frankhamand@gmail.com>
2017-01-31 15:01:07 +0000

    Add migrations for django 1.7

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-email',
-      version='1.3.0',
+      version='1.4.3',
       description='An email alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url= 'https://github.com/bonniejools/cabot-alert-email/tarball/1.3.0'
+      download_url= 'https://github.com/cabotapp/cabot-alert-email/archive/1.4.3.zip',
      )
